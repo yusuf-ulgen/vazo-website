42a405bbf003774e96877289cb1353be917b6c0c
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-08-25 21:01:34 +0300

    chore: sync workspace changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from '@/app/App';
 import '@/app/styles/index.css';
+import "@mbbe-software/dev-support/auto";
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
